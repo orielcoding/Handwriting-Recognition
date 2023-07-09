de4a4a22453a2a3c76c4df2e004e82ca6ab94684
Oriel Singer <oriel2486@gmail.com>
2023-07-09 21:41:21 +0300

    slight modifications for more accessible preprocessing.

--- MS2/preprocessing.py
+++ MS2/preprocessing.py
@@ -20,7 +20,9 @@ def padding(img, original_width, original_height, target_width, target_height):
     padding_width_start = int((target_width - original_width) / 2)
     padding_width_end = padding_width_start + original_width
 
-    padded_img = np.full([target_height, target_width, 1], 255) # creates an "empty" image (white pixels)
+    num_channels = img.shape[2]  # Number of color channels in the image
+
+    padded_img = np.full([target_height, target_width, num_channels], 255)  # Create an "empty" image (white pixels)
     padded_img[padding_height_start:padding_height_end, padding_width_start:padding_width_end, :] = img
 
     return padded_img
